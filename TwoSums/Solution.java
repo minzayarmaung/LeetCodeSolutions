f2bb91d017276cf574436ecddd764a5623fe1605
Min Zayar Maung <minzayarmaung2002@gmail.com>
2024-08-29 22:09:12 +0630

    twosums

---
+++ TwoSums/Solution.java
@@ -0,0 +1,26 @@
+package TwoSums;
+
+public class Solution {
+
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 2, 7, 3, 15 };
+        int[] result = Solution.twoSum(nums, 10);
+
+        if (result != null) {
+            System.out.println("Indices: " + result[0] + ", " + result[1]);
+        } else {
+            System.out.println("No solution found.");
+        }
+    }
+}
