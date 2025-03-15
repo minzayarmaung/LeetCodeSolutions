4ecc536be03e4aa78b6237001bab48d6ba9bb7af
Min Zayar Maung <minzayarmaung2002@gmail.com>
2025-03-15 19:13:18 +0630

    add hollow square

---
+++ Easy_Level/Hollow_Square/Hollow_Square.java
@@ -0,0 +1,11 @@
+public class Hollow_Square {
+    public static void main(String[] args) {
+        drawSqure(5);
+    }
+
+    private static void drawSqure(int n) {
+        for(int i=0; i<n ; i++){
+            
+        }
+    }
+}
